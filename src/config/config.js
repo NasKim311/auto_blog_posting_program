16aaf1d29481c74a4b038cc78e530aa68559a992
김남수 <skatn0311@gmail.com>
2025-04-29 22:48:35 +0900

    공공 포털 url 및 apikey 추가

--- src/config/config.js
+++ src/config/config.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 module.exports = {
     loginURL: 'https://nid.naver.com/nidlogin.login',
     blogMainURL: 'https://blog.naver.com',
-    writePostURL: 'https://blog.naver.com/PostWrite.naver',
+    writePostURL: `https://blog.naver.com/${process.env.NAVER_ID}?Redirect=Write`,
     contentSources: [
-        'https://www.example.com/news', // 컨텐츠를 가져올 소스 URL
-        'https://www.example2.com/articles',
+        'https://www.data.go.kr/tcs/dss/selectDataSetList.do', // 공공데이터 포털
+        'https://api.visitkorea.or.kr/#/sample', // 한국관광공사 API
+        'https://www.weather.go.kr/w/index.do', // 기상청 날씨 정보
     ],
+    apiKeys: {
+        tourApi: process.env.TOUR_API_KEY,
+        weatherApi: process.env.WEATHER_API_KEY,
+    },
     categories: ['일상', '여행', '맛집', '리뷰'], // 블로그 카테고리 목록
     postInterval: 3600000, // 포스팅 간격 (1시간)
     maxPostsPerDay: 5, // 하루 최대 포스팅 수
